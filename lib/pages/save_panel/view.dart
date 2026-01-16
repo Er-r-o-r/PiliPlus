8e18731702d43bfd34fd4813182894e78d94826b
Er-r-o-r <1773127066@qq.com>
2026-01-16 20:24:46 +0800

    opt: remove logo from save panel for cleaner UI

--- lib/pages/save_panel/view.dart
+++ lib/pages/save_panel/view.dart
@@ -531,16 +531,6 @@ class _SavePanelState extends State<SavePanel> {
                                           ],
                                         ),
                                       ),
-                                    Align(
-                                      alignment: Alignment.centerLeft,
-                                      child: Image.asset(
-                                        'assets/images/logo/logo_2.png',
-                                        width: 100,
-                                        cacheWidth: 100.cacheSize(context),
-                                        color:
-                                            theme.colorScheme.onSurfaceVariant,
-                                      ),
-                                    ),
                                   ],
                                 )
                               : const SizedBox(height: 12),
